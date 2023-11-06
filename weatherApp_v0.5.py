3158cf6bd2b3fcc36fe3f2197d51127f73230fde
gyojincompany <luckyred@gmail.com>
2023-11-06 09:38:33 +0900

    날씨 이미지출력 기능 추가

--- weatherApp_v0.5.py
+++ weatherApp_v0.5.py
@@ -27,20 +27,50 @@ class WeatherWin(QMainWindow, form_class):
 
         area_text = weather_soup.find('h2', {'class': 'title'}).text
         # 날씨를 조회하려는 지역주소 가져오기
-        self.area_label.setText(area_text) # area_label 레이블 자리에 area_text를 출력
+
         today_temperature = weather_soup.find('div', {'class': 'temperature_text'}).text  # 현재온도
         today_temperature = today_temperature[6:11]
-        self.temper_label.setText(today_temperature) # temper_label 레이블 자리에 today_temperature를 출력
+
         yesterday_weathertext = weather_soup.find('p', {'class': 'summary'}).text  # 어제온도와 현재온도의 비교
         yesterday_weathertext = yesterday_weathertext[:13].strip()
-        self.yesterdayTemper_label.setText(yesterday_weathertext)
+
         sense_temperature = weather_soup.find('div', {'class': 'weather_info'}).find('dl', {'class': 'summary_list'}).find('dd', {'class': 'desc'}).text # 체감온도
         dust_info = weather_soup.select('ul.today_chart_list>li')
         dust1_info = dust_info[0].find('span', {'class': 'txt'}).text  # 미세먼지 정보
         dust2_info = dust_info[1].find('span', {'class': 'txt'}).text  # 초미세먼지 정보
+
+        today_weathertext = weather_soup.find('span', {'class': 'weather before_slash'}).text  # 현재온도 텍스트
+
+        self.setWeatherImage(today_weathertext)
+        self.area_label.setText(area_text)  # area_label 레이블 자리에 area_text를 출력
+        self.temper_label.setText(today_temperature)  # temper_label 레이블 자리에 today_temperature를 출력
+        self.yesterdayTemper_label.setText(yesterday_weathertext)
         self.senseTemper_label.setText(sense_temperature) # 체감온도 출력
         self.dust1_label.setText(dust1_info) #미세먼지정보 출력
         self.dust2_label.setText(dust2_info) #초미세먼지정보 출력
+
+    def setWeatherImage(self, weatherText):
+        # 날씨 종류 : 흐림, 맑음, 눈, 비, 구름많음, 기타
+        if weatherText == '흐림':
+            weatherImage = QPixmap("img/cloud.png")
+            self.weather_imageLabel.setPixmap(QPixmap(weatherImage))
+        elif weatherText == '맑음':
+            weatherImage = QPixmap("img/sun.png")
+            self.weather_imageLabel.setPixmap(QPixmap(weatherImage))
+        elif weatherText == '눈':
+            weatherImage = QPixmap("img/snow.png")
+            self.weather_imageLabel.setPixmap(QPixmap(weatherImage))
+        elif weatherText == '비':
+            weatherImage = QPixmap("img/rain.png")
+            self.weather_imageLabel.setPixmap(QPixmap(weatherImage))
+        elif weatherText == '구름많음':
+            weatherImage = QPixmap("img/cloud.png")
+            self.weather_imageLabel.setPixmap(QPixmap(weatherImage))
+        else:
+            self.weather_imageLabel.setText(weatherText)
+
+
+
 
 
 
